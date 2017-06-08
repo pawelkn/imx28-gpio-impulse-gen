a7ac76184b0c80c872bdd672225e4834550f88be
Pawel Kniola <p.kniola@xtrack.local>
2017-06-08 14:56:15 +0200

    Driver is ready

--- gpio-pulse-generator.c
+++ gpio-pulse-generator.c
@@ -1,9 +1,7 @@
 /*
- * gpio_pulse_generator.c
- *
  * (c) 2017 Paweł Knioła <pawel.kn@gmail.com>
  *
- * Generic GPIO impulse generator.
+ * Generic GPIO pulse generator.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -20,48 +18,67 @@
 #include <linux/of.h>
 #include <linux/of_platform.h>
 #include <linux/of_gpio.h>
+#include <linux/of_address.h>
+#include <linux/of_irq.h>
 #include <linux/pm.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <linux/time.h>
 
 #include <asm/uaccess.h>
+#include <asm/io.h>
 
 #define DRV_NAME "gpio-pulse-generator"
 
-struct gpio_pulse_generator_platform_data {
+#define HW_TIMROT_TIMCTRL_REG(n)        (0x20 + (n) * 0x40)
+#define HW_TIMROT_TIMCTRL_REG_SET(n)    (HW_TIMROT_TIMCTRL_REG(n) + 4)
+#define HW_TIMROT_TIMCTRL_REG_CLR(n)    (HW_TIMROT_TIMCTRL_REG(n) + 8)
+#define HW_TIMROT_FIXED_COUNT_REG(n)    (0x40 + (n) * 0x40)
+
+#define TIMROT_TIMCTRL_SELECT_32K       (0xb)
+#define TIMROT_TIMCTRL_ALWAYS_TICK      (0xf)
+#define TIMROT_TIMCTRL_RELOAD           (1 << 6)
+#define TIMROT_TIMCTRL_UPDATE           (1 << 7)
+#define TIMROT_TIMCTRL_IRQ_EN           (1 << 14)
+#define TIMROT_TIMCTRL_IRQ              (1 << 15)
+
+#define TIMROT_N_DEFAULT    2
+#define TIMEOUT_DEFAULT     60000
+
+struct gpg_platform_data {
     int gpio;
     bool inverted;
-    u32 width_ms;
-    u32 delay_ms;
+
+    u32 pulse_width;
+    u32 pulse_delay;
+ 
+    u32 timrot_n;
+    u32 timrot_irq;
+    void __iomem *timrot_base;
 };
 
-struct gpio_pulse_generator {
-    const struct gpio_pulse_generator_platform_data *pdata;
+struct gpg_hw {
+    const struct gpg_platform_data *pdata;
     struct miscdevice miscdev;
-    struct delayed_work work;
 
-    int irq;
     u64 count;
-    s64 last_ns;
-    bool last_state;
-    bool last_stable_state;
+    bool state;
 };
 
-static int gpio_pulse_generator_open(struct inode *inode, struct file *file)
+static int gpg_open(struct inode *inode, struct file *file)
 {
-   // dummy open function must be declared due to bug in kernel 3.16
+   /* dummy open function must be declared due to bug in kernel 3.16 */
    return 0;
 }
 
-static ssize_t gpio_pulse_generator_read(struct file *file, char __user * userbuf, size_t count, loff_t * ppos)
+static ssize_t gpg_read(struct file *file, char __user * userbuf, size_t count, loff_t * ppos)
 {
     struct miscdevice *miscdev = file->private_data;
     struct device *dev = miscdev->parent;
-    struct gpio_pulse_generator *generator = dev_get_drvdata(dev);
+    struct gpg_hw *hw = dev_get_drvdata(dev);
 
     char buf[22];
-    int len = sprintf(buf, "%llu\n", generator->count);
+    int len = sprintf(buf, "%llu\n", hw->count);
 
     if ((len < 0) || (len > count))
         return -EINVAL;
@@ -76,252 +93,212 @@ static ssize_t gpio_pulse_generator_read(struct file *file, char __user * userbu
     return len;
 }
 
-static ssize_t gpio_pulse_generator_write(struct file *file, const char __user * userbuf, size_t count, loff_t * ppos)
+static ssize_t gpg_write(struct file *file, const char __user * userbuf, size_t len, loff_t * ppos)
 {
     struct miscdevice *miscdev = file->private_data;
     struct device *dev = miscdev->parent;
-    struct gpio_pulse_generator *generator = dev_get_drvdata(dev);
+    struct gpg_hw *hw = dev_get_drvdata(dev);
 
     u64 increment;
-    if (kstrtoull_from_user(userbuf, count, 0, &generator->count))
+    if (kstrtoull_from_user(userbuf, len, 0, &increment))
         return -EINVAL;
 
-    //generator->count += increment;
-    return count;
+    hw->count += increment;    
+    return len;
 }
 
-static struct file_operations gpio_pulse_generator_fops = {
-    .owner = THIS_MODULE,
-    .open = gpio_pulse_generator_open,
-    .read = gpio_pulse_generator_read,
-    .write = gpio_pulse_generator_write,
+static struct file_operations gpg_fops = {
+    .owner  = THIS_MODULE,
+    .open   = gpg_open,
+    .read   = gpg_read,
+    .write  = gpg_write,
 };
 
-static bool gpio_pulse_generator_get_state(const struct gpio_pulse_generator_platform_data *pdata)
+static irqreturn_t gpg_irq(int irq, void *dev_id)
 {
-    bool state = gpio_get_value(pdata->gpio);
-    if (pdata->inverted)
-        state = !state;
+    struct gpg_hw *hw = dev_id;
+    const struct gpg_platform_data *pdata = hw->pdata;
 
-    return state;
-}
+    if (hw->count > 0 ) { 
+        if (hw->state)
+            hw->count--;
 
-static s64 gpio_pulse_generator_get_time_nsec(void)
-{
-    struct timespec ts;
-    getnstimeofday (&ts);
-    return timespec_to_ns (&ts);
-}
-
-static void gpio_pulse_generator_process_state_change(struct gpio_pulse_generator *generator)
-{
-    bool state;
-    s64 current_ns;
-    s64 delta_ns = 0;
-    s64 debounce_ns;
-
-    state = gpio_pulse_generator_get_state (generator->pdata);
-    current_ns = gpio_pulse_generator_get_time_nsec();
-    delta_ns = current_ns - generator->last_ns;
-    debounce_ns = (s64)generator->pdata->delay_ms * NSEC_PER_USEC;
-
-    if (delta_ns > debounce_ns) {
-        if (generator->last_state && !generator->last_stable_state)
-            generator->count++;
-
-        generator->last_stable_state = generator->last_state;
+       hw->state = !hw->state;
+       gpio_set_value(pdata->gpio, hw->state ^ pdata->inverted);
     }
 
-    generator->last_state = state;
-    generator->last_ns = current_ns;
-}
+    /* restart timer */
+    writel(hw->state ? pdata->pulse_width : pdata->pulse_delay, 
+        pdata->timrot_base + HW_TIMROT_FIXED_COUNT_REG(pdata->timrot_n));
 
-static void gpio_pulse_generator_delayed_work(struct work_struct *work)
-{
-    struct gpio_pulse_generator *generator = container_of(work, struct gpio_pulse_generator, work.work);
-    gpio_pulse_generator_process_state_change (generator);
-}
-
-static irqreturn_t gpio_pulse_generator_irq(int irq, void *dev_id)
-{
-    struct gpio_pulse_generator *generator = dev_id;
-    gpio_pulse_generator_process_state_change (generator);
-
-    if (delayed_work_pending (&generator->work))
-        cancel_delayed_work (&generator->work);
-
-    schedule_delayed_work (&generator->work,
-        usecs_to_jiffies (generator->pdata->delay_ms));
-
+    /* acknowledge the interrupt */
+    writel(TIMROT_TIMCTRL_IRQ, 
+        pdata->timrot_base + HW_TIMROT_TIMCTRL_REG_CLR(pdata->timrot_n));
     return IRQ_HANDLED;
 }
 
-static const struct of_device_id gpio_pulse_generator_of_match[] = {
-    { .compatible = "gpio-pulse-generator", },
+static const struct of_device_id gpg_of_match[] = {
+    { .compatible = DRV_NAME, },
     { },
 };
-MODULE_DEVICE_TABLE(of, gpio_pulse_generator_of_match);
+MODULE_DEVICE_TABLE(of, gpg_of_match);
 
-static struct gpio_pulse_generator_platform_data *gpio_pulse_generator_parse_dt(struct device *dev)
+static struct gpg_platform_data *gpg_parse_dt(struct device *dev)
 {
-    const struct of_device_id *of_id =
-                of_match_device(gpio_pulse_generator_of_match, dev);
-    struct device_node *np = dev->of_node;
-    struct gpio_pulse_generator_platform_data *pdata;
     enum of_gpio_flags flags;
+    struct gpg_platform_data *pdata;
+    const struct of_device_id *of_id;
+    struct device_node *np;
     int err;
+
+    /* parse driver device tree */
+    of_id = of_match_device(gpg_of_match, dev);
+    np = dev->of_node;
 
     if (!of_id || !np)
         return NULL;
 
-    pdata = kzalloc(sizeof(struct gpio_pulse_generator_platform_data), GFP_KERNEL);
+    pdata = kzalloc(sizeof(struct gpg_platform_data), GFP_KERNEL);
     if (!pdata)
         return ERR_PTR(-ENOMEM);
 
     pdata->gpio = of_get_gpio_flags(np, 0, &flags);
     pdata->inverted = flags & OF_GPIO_ACTIVE_LOW;
     
-    err = of_property_read_u32(np, "width-ms", &pdata->width_ms);
+    err = of_property_read_u32(np, "pulse-width", &pdata->pulse_width);
     if (err)
-        pdata->width_ms = 5;
+        pdata->pulse_width = TIMEOUT_DEFAULT;
 
-
-    err = of_property_read_u32(np, "delay-ms", &pdata->delay_ms);
+    err = of_property_read_u32(np, "pulse-delay", &pdata->pulse_delay);
     if (err)
-        pdata->delay_ms = 5;
+        pdata->pulse_delay = TIMEOUT_DEFAULT;
+
+    err = of_property_read_u32(np, "timrot-number", &pdata->timrot_n);
+    if (err)
+        pdata->timrot_n = TIMROT_N_DEFAULT;
+
+    /* parse timrot device tree */
+    np = of_find_compatible_node(NULL, NULL, "fsl,timrot");
+    pdata->timrot_base = of_iomap(np, 0);
+    if (!pdata->timrot_base)
+        return ERR_PTR(-ENOMEM);
+
+    pdata->timrot_irq = irq_of_parse_and_map(np, pdata->timrot_n);
+    if (pdata->timrot_irq < 0)
+        return ERR_PTR(-ENOMEM);
 
     return pdata;
 }
 
-static int gpio_pulse_generator_probe(struct platform_device *pdev)
+static int gpg_probe(struct platform_device *pdev)
 {
     struct device *dev = &pdev->dev;
-    const struct gpio_pulse_generator_platform_data *pdata = dev_get_platdata(dev);
-    struct gpio_pulse_generator *generator;
+    const struct gpg_platform_data *pdata = dev_get_platdata(dev);
+    struct gpg_hw *hw;
     int err;
-
+    
+    /* Read platform data */
     if (!pdata) {
-        pdata = gpio_pulse_generator_parse_dt(dev);
+        pdata = gpg_parse_dt(dev);
         if (IS_ERR(pdata))
             return PTR_ERR(pdata);
 
         if (!pdata) {
-            dev_err(dev, "missing platform data\n");
+            dev_err(dev, "missing or invalid platform data\n");
             return -EINVAL;
         }
     }
-
-    generator = kzalloc(sizeof(struct gpio_pulse_generator), GFP_KERNEL);
-    if (!generator) {
+    
+    /* Create device data struct */
+    hw = kzalloc(sizeof(struct gpg_hw), GFP_KERNEL);
+    if (!hw) {
         err = -ENOMEM;
         goto exit_free_mem;
     }
 
-    generator->pdata = pdata;
-    generator->count = 0;
-
+    hw->pdata = pdata;
+    hw->count = 0;
+    hw->state = 0;
+    
+    dev_set_drvdata(dev, hw);
+    
+    /* Setup gpio */
     err = gpio_request_one(pdata->gpio, GPIOF_IN, dev_name(dev));
     if (err) {
         dev_err(dev, "unable to request GPIO %d\n", pdata->gpio);
         goto exit_free_mem;
     }
+    
+    gpio_direction_output(pdata->gpio, 1);
+    gpio_set_value(pdata->gpio, hw->state ^ pdata->inverted);
 
-    generator->last_stable_state = gpio_pulse_generator_get_state (pdata);
-    generator->last_state = generator->last_stable_state;
-    generator->irq = gpio_to_irq (pdata->gpio);
-    generator->last_ns = gpio_pulse_generator_get_time_nsec();
+    /* Setup timer */
+    writel(TIMROT_TIMCTRL_UPDATE | TIMROT_TIMCTRL_ALWAYS_TICK | TIMROT_TIMCTRL_IRQ_EN,
+           pdata->timrot_base + HW_TIMROT_TIMCTRL_REG(pdata->timrot_n));
 
-    err = request_irq(generator->irq, &gpio_pulse_generator_irq,
-                      IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING,
-                      DRV_NAME, generator);
+    /* Setup timer irq */
+    err = request_irq(pdata->timrot_irq, &gpg_irq, 0, DRV_NAME, hw);    
     if (err) {
-        dev_err(dev, "unable to request IRQ %d\n", generator->irq);
+        dev_err(dev, "failed to register timrot_irq\n");
         goto exit_free_gpio;
     }
 
-    dev_set_drvdata(dev, generator);
+    /* Run timer */
+    writel(TIMEOUT_DEFAULT, pdata->timrot_base + HW_TIMROT_FIXED_COUNT_REG(pdata->timrot_n));
 
-    generator->miscdev.minor  = MISC_DYNAMIC_MINOR;
-    generator->miscdev.name   = dev_name(dev);
-    generator->miscdev.fops   = &gpio_pulse_generator_fops;
-    generator->miscdev.parent = dev;
+    /* Setup misc devcie */
+    hw->miscdev.minor  = MISC_DYNAMIC_MINOR;
+    hw->miscdev.name   = dev_name(dev);
+    hw->miscdev.fops   = &gpg_fops;
+    hw->miscdev.parent = dev;
 
-    INIT_DELAYED_WORK(&generator->work, gpio_pulse_generator_delayed_work);
-
-    err = misc_register(&generator->miscdev);
+    err = misc_register(&hw->miscdev);
     if (err) {
         dev_err(dev, "failed to register misc device\n");
         goto exit_free_irq;
     }
 
-    dev_info(dev, "registered new misc device %s\n", generator->miscdev);
+    dev_info(dev, "registered new misc device %s\n", hw->miscdev.name);
     return 0;
 
 exit_free_irq:
-    free_irq(generator->irq, generator);
+    free_irq(pdata->timrot_irq, hw);
 exit_free_gpio:
     gpio_free(pdata->gpio);
 exit_free_mem:
-    kfree(generator);
+    kfree(hw);
     if (!dev_get_platdata(&pdev->dev))
         kfree(pdata);
 
     return err;
 }
 
-static int gpio_pulse_generator_remove(struct platform_device *pdev)
+static int gpg_remove(struct platform_device *pdev)
 {
-    struct gpio_pulse_generator *generator = platform_get_drvdata(pdev);
-    const struct gpio_pulse_generator_platform_data *pdata = generator->pdata;
+    struct gpg_hw *hw = platform_get_drvdata(pdev);
+    const struct gpg_platform_data *pdata = hw->pdata;
 
     device_init_wakeup(&pdev->dev, false);
 
-    if (delayed_work_pending(&generator->work))
-        cancel_delayed_work(&generator->work);
-
-    misc_deregister(&generator->miscdev);
-    free_irq(generator->irq, generator);
+    misc_deregister(&hw->miscdev);
+    free_irq(pdata->timrot_irq, hw);
     gpio_free(pdata->gpio);
-    kfree(generator);
+    kfree(hw);
     if (!dev_get_platdata(&pdev->dev))
         kfree(pdata);
 
     return 0;
 }
 
-static int gpio_pulse_generator_suspend(struct device *dev)
-{
-    struct gpio_pulse_generator *generator = dev_get_drvdata(dev);
-
-    if (device_may_wakeup(dev))
-        disable_irq_wake(generator->irq);
-
-    return 0;
-}
-
-static int gpio_pulse_generator_resume(struct device *dev)
-{
-    struct gpio_pulse_generator *generator = dev_get_drvdata(dev);
-
-    if (device_may_wakeup(dev))
-        enable_irq_wake(generator->irq);
-
-    return 0;
-}
-
-static SIMPLE_DEV_PM_OPS(gpio_pulse_generator_pm_ops,
-         gpio_pulse_generator_suspend, gpio_pulse_generator_resume);
-
-static struct platform_driver gpio_pulse_generator_driver = {
-    .probe		= gpio_pulse_generator_probe,
-    .remove		= gpio_pulse_generator_remove,
-    .driver		= {
-        .name	= DRV_NAME,
-        .pm	= &gpio_pulse_generator_pm_ops,
-        .of_match_table = of_match_ptr(gpio_pulse_generator_of_match),
+static struct platform_driver gpg_driver = {
+    .probe       = gpg_probe,
+    .remove      = gpg_remove,
+    .driver      = {
+        .name    = DRV_NAME,
+        .of_match_table = of_match_ptr(gpg_of_match),
     }
 };
-module_platform_driver(gpio_pulse_generator_driver);
+module_platform_driver(gpg_driver);
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("Generic GPIO pulse generator driver");
