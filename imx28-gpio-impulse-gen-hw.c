940dffe05f9db69e26e6a1e3d97c92a40598e051
Pawel Kniola <p.kniola@xtrack.local>
2017-06-13 13:03:49 +0200

    Code refractoring: replace spaces by tabs

--- imx28-gpio-impulse-gen-hw.c
+++ imx28-gpio-impulse-gen-hw.c
@@ -36,28 +36,28 @@
 #define DRV_NAME "imx28-gpio-impulse-gen"
 
 struct imx28_gpio_impulse_gen_platform_data {
-    int gpio;
+	int gpio;
 
-    u32 timrot;
-    u32 timrot_irq;
-    u32 timrot_fixed_count;
+	u32 timrot;
+	u32 timrot_irq;
+	u32 timrot_fixed_count;
 
-    void __iomem *timrot_base;
-    void __iomem *pinctrl_base;
-    void __iomem *icoll_base;
+	void __iomem *timrot_base;
+	void __iomem *pinctrl_base;
+	void __iomem *icoll_base;
 };
 
 struct imx28_gpio_impulse_gen_hw {
-    const struct imx28_gpio_impulse_gen_platform_data *pdata;
-    struct miscdevice miscdev;
+	const struct imx28_gpio_impulse_gen_platform_data *pdata;
+	struct miscdevice miscdev;
 
-    u64 counter;
-    bool state;
+	u64 counter;
+	bool state;
 };
 
 extern unsigned char imx28_gpio_impulse_gen_fiq_handler, imx28_gpio_impulse_gen_fiq_handler_end;
 static struct fiq_handler imx28_gpio_impulse_gen_fh = {
-    .name = DRV_NAME
+	.name = DRV_NAME
 };
 
 static int imx28_gpio_impulse_gen_open(struct inode *inode, struct file *file)
@@ -68,236 +68,236 @@ static int imx28_gpio_impulse_gen_open(struct inode *inode, struct file *file)
 
 static ssize_t imx28_gpio_impulse_gen_read(struct file *file, char __user * userbuf, size_t count, loff_t * ppos)
 {
-    struct miscdevice *miscdev = file->private_data;
-    struct device *dev = miscdev->parent;
-    struct imx28_gpio_impulse_gen_hw *hw = dev_get_drvdata(dev);
+	struct miscdevice *miscdev = file->private_data;
+	struct device *dev = miscdev->parent;
+	struct imx28_gpio_impulse_gen_hw *hw = dev_get_drvdata(dev);
 
-    char buf[22];
-    int len = sprintf(buf, "%llu\n", hw->counter);
+	char buf[22];
+	int len = sprintf(buf, "%llu\n", hw->counter);
 
-    if ((len < 0) || (len > count))
-        return -EINVAL;
+	if ((len < 0) || (len > count))
+		return -EINVAL;
 
-    if (*ppos != 0)
-        return 0;
+	if (*ppos != 0)
+		return 0;
 
-    if (copy_to_user(userbuf, buf, len))
-        return -EINVAL;
+	if (copy_to_user(userbuf, buf, len))
+		return -EINVAL;
 
-    *ppos = len;
-    return len;
+	*ppos = len;
+	return len;
 }
 
 static ssize_t imx28_gpio_impulse_gen_write(struct file *file, const char __user * userbuf, size_t count, loff_t * ppos)
 {
-    struct miscdevice *miscdev = file->private_data;
-    struct device *dev = miscdev->parent;
-    struct imx28_gpio_impulse_gen_hw *hw = dev_get_drvdata(dev);
+	struct miscdevice *miscdev = file->private_data;
+	struct device *dev = miscdev->parent;
+	struct imx28_gpio_impulse_gen_hw *hw = dev_get_drvdata(dev);
 
-    u64 increment;
-    if (kstrtoull_from_user(userbuf, count, 0, &increment))
-        return -EINVAL;
+	u64 increment;
+	if (kstrtoull_from_user(userbuf, count, 0, &increment))
+		return -EINVAL;
 
-    hw->counter += increment;
-    return count;
+	hw->counter += increment;
+	return count;
 }
 
 static struct file_operations imx28_gpio_impulse_gen_fops = {
-    .owner  = THIS_MODULE,
-    .open   = imx28_gpio_impulse_gen_open,
-    .read   = imx28_gpio_impulse_gen_read,
-    .write  = imx28_gpio_impulse_gen_write,
+	.owner = THIS_MODULE,
+	.open  = imx28_gpio_impulse_gen_open,
+	.read  = imx28_gpio_impulse_gen_read,
+	.write = imx28_gpio_impulse_gen_write,
 };
 
 static const struct of_device_id imx28_gpio_impulse_gen_of_match[] = {
-    { .compatible = DRV_NAME, },
-    { },
+	{ .compatible = DRV_NAME, },
+	{ },
 };
 MODULE_DEVICE_TABLE(of, imx28_gpio_impulse_gen_of_match);
 
 static struct imx28_gpio_impulse_gen_platform_data *imx28_gpio_impulse_gen_parse_dt(struct device *dev)
 {
-    enum of_gpio_flags flags;
-    struct imx28_gpio_impulse_gen_platform_data *pdata;
-    const struct of_device_id *of_id;
-    struct device_node *np;
-    int err;
+	enum of_gpio_flags flags;
+	struct imx28_gpio_impulse_gen_platform_data *pdata;
+	const struct of_device_id *of_id;
+	struct device_node *np;
+	int err;
 
-    /* parse driver device tree */
-    of_id = of_match_device(imx28_gpio_impulse_gen_of_match, dev);
-    np = dev->of_node;
+	/* parse driver device tree */
+	of_id = of_match_device(imx28_gpio_impulse_gen_of_match, dev);
+	np = dev->of_node;
 
-    if (!of_id || !np)
-        return NULL;
+	if (!of_id || !np)
+		return NULL;
 
-    pdata = devm_kzalloc(dev, sizeof(struct imx28_gpio_impulse_gen_platform_data), GFP_KERNEL);
-    if (!pdata)
-        return ERR_PTR(-ENOMEM);
+	pdata = devm_kzalloc(dev, sizeof(struct imx28_gpio_impulse_gen_platform_data), GFP_KERNEL);
+	if (!pdata)
+		return ERR_PTR(-ENOMEM);
 
-    pdata->gpio = of_get_gpio_flags(np, 0, &flags);
+	pdata->gpio = of_get_gpio_flags(np, 0, &flags);
 
-    err = of_property_read_u32(np, "timrot", &pdata->timrot);
-    if (err)
-        pdata->timrot = TIMROT_DEFAULT;
+	err = of_property_read_u32(np, "timrot", &pdata->timrot);
+	if (err)
+		pdata->timrot = TIMROT_DEFAULT;
 
-    err = of_property_read_u32(np, "timrot-fixed-count", &pdata->timrot_fixed_count);
-    if (err)
-        pdata->timrot_fixed_count = TIMROT_FIXED_COUNT_DEFAULT;
+	err = of_property_read_u32(np, "timrot-fixed-count", &pdata->timrot_fixed_count);
+	if (err)
+		pdata->timrot_fixed_count = TIMROT_FIXED_COUNT_DEFAULT;
 
-    /* parse timrot device tree */
-    np = of_find_compatible_node(NULL, NULL, "fsl,imx28-timrot");
-    pdata->timrot_base = of_iomap(np, 0);
-    if (!pdata->timrot_base)
-        return ERR_PTR(-ENOMEM);
+	/* parse timrot device tree */
+	np = of_find_compatible_node(NULL, NULL, "fsl,imx28-timrot");
+	pdata->timrot_base = of_iomap(np, 0);
+	if (!pdata->timrot_base)
+		return ERR_PTR(-ENOMEM);
 
-    pdata->timrot_irq = irq_of_parse_and_map(np, pdata->timrot);
-    if (pdata->timrot_irq < 0)
-        return ERR_PTR(-ENOMEM);
+	pdata->timrot_irq = irq_of_parse_and_map(np, pdata->timrot);
+	if (pdata->timrot_irq < 0)
+		return ERR_PTR(-ENOMEM);
 
-    /* parse pin control device tree */
-    np = of_find_compatible_node(NULL, NULL, "fsl,imx28-pinctrl");
-    pdata->pinctrl_base = of_iomap(np, 0);
-    if (!pdata->pinctrl_base)
-        return ERR_PTR(-ENOMEM);
+	/* parse pin control device tree */
+	np = of_find_compatible_node(NULL, NULL, "fsl,imx28-pinctrl");
+	pdata->pinctrl_base = of_iomap(np, 0);
+	if (!pdata->pinctrl_base)
+		return ERR_PTR(-ENOMEM);
 
-    /* parse interrupt controller device tree */
-    np = of_find_compatible_node(NULL, NULL, "fsl,imx28-icoll");
-    pdata->icoll_base = of_iomap(np, 0);
-    if (!pdata->icoll_base)
-        return ERR_PTR(-ENOMEM);
+	/* parse interrupt controller device tree */
+	np = of_find_compatible_node(NULL, NULL, "fsl,imx28-icoll");
+	pdata->icoll_base = of_iomap(np, 0);
+	if (!pdata->icoll_base)
+		return ERR_PTR(-ENOMEM);
 
-    return pdata;
+	return pdata;
 }
 
 static int imx28_gpio_impulse_gen_probe(struct platform_device *pdev)
 {
-    struct device *dev = &pdev->dev;
-    const struct imx28_gpio_impulse_gen_platform_data *pdata = dev_get_platdata(dev);
-    struct imx28_gpio_impulse_gen_hw *hw;
-    struct irq_data* irqd;
-    struct pt_regs regs;
-    long gpio_pin, gpio_bank;
-    int err;
+	struct device *dev = &pdev->dev;
+	const struct imx28_gpio_impulse_gen_platform_data *pdata = dev_get_platdata(dev);
+	struct imx28_gpio_impulse_gen_hw *hw;
+	struct irq_data* irqd;
+	struct pt_regs regs;
+	long gpio_pin, gpio_bank;
+	int err;
 
-    /* read platform data */
-    if (!pdata) {
-        pdata = imx28_gpio_impulse_gen_parse_dt(dev);
-        if (IS_ERR(pdata))
-            return PTR_ERR(pdata);
+	/* read platform data */
+	if (!pdata) {
+		pdata = imx28_gpio_impulse_gen_parse_dt(dev);
+		if (IS_ERR(pdata))
+			return PTR_ERR(pdata);
 
-        if (!pdata) {
-            dev_err(dev, "missing or invalid platform data\n");
-            return -EINVAL;
-        }
-    }
+		if (!pdata) {
+			dev_err(dev, "missing or invalid platform data\n");
+			return -EINVAL;
+		}
+	}
 
-    /* create device data struct */
-    hw = devm_kzalloc(dev, sizeof(struct imx28_gpio_impulse_gen_hw), GFP_KERNEL);
-    if (!hw) {
-        err = -ENOMEM;
-        goto exit_free_mem;
-    }
+	/* create device data struct */
+	hw = devm_kzalloc(dev, sizeof(struct imx28_gpio_impulse_gen_hw), GFP_KERNEL);
+	if (!hw) {
+		err = -ENOMEM;
+		goto exit_free_mem;
+	}
 
-    hw->pdata = pdata;
-    hw->counter = 0;
-    hw->state = 0;
+	hw->pdata = pdata;
+	hw->counter = 0;
+	hw->state = 0;
 
-    dev_set_drvdata(dev, hw);
+	dev_set_drvdata(dev, hw);
 
-    /* setup gpio */
-    err = gpio_request_one(pdata->gpio, GPIOF_IN, dev_name(dev));
-    if (err) {
-        dev_err(dev, "unable to request GPIO %d\n", pdata->gpio);
-        goto exit_free_mem;
-    }
+	/* setup gpio */
+	err = gpio_request_one(pdata->gpio, GPIOF_IN, dev_name(dev));
+	if (err) {
+		dev_err(dev, "unable to request GPIO %d\n", pdata->gpio);
+		goto exit_free_mem;
+	}
 
-    gpio_bank = pdata->gpio / 32;
-    gpio_pin = (1 << (pdata->gpio % 32));
+	gpio_bank = pdata->gpio / 32;
+	gpio_pin = (1 << (pdata->gpio % 32));
 
-    gpio_direction_output(pdata->gpio, 1);
-    gpio_set_value(pdata->gpio, 0);
+	gpio_direction_output(pdata->gpio, 1);
+	gpio_set_value(pdata->gpio, 0);
 
-    /* setup timer */
-    writel(TIMROT_TIMCTRL_UPDATE | TIMROT_TIMCTRL_ALWAYS_TICK |
-        TIMROT_TIMCTRL_RELOAD | TIMROT_TIMCTRL_IRQ_EN,
-        pdata->timrot_base + HW_TIMROT_TIMCTRL_REG(pdata->timrot));
+	/* setup timer */
+	writel(TIMROT_TIMCTRL_UPDATE | TIMROT_TIMCTRL_ALWAYS_TICK |
+		TIMROT_TIMCTRL_RELOAD | TIMROT_TIMCTRL_IRQ_EN,
+		pdata->timrot_base + HW_TIMROT_TIMCTRL_REG(pdata->timrot));
 
-    /* setup FIQ */
-    err = claim_fiq(&imx28_gpio_impulse_gen_fh);
-    if (err) {
-        dev_err(dev, "failed to register timrot FIQ\n");
-        goto exit_free_gpio;
-    }
+	/* setup FIQ */
+	err = claim_fiq(&imx28_gpio_impulse_gen_fh);
+	if (err) {
+		dev_err(dev, "failed to register timrot FIQ\n");
+		goto exit_free_gpio;
+	}
 
-    set_fiq_handler(&imx28_gpio_impulse_gen_fiq_handler,
-        &imx28_gpio_impulse_gen_fiq_handler_end - &imx28_gpio_impulse_gen_fiq_handler);
+	set_fiq_handler(&imx28_gpio_impulse_gen_fiq_handler,
+		&imx28_gpio_impulse_gen_fiq_handler_end - &imx28_gpio_impulse_gen_fiq_handler);
 
-    regs.uregs[reg_timctrl] = (long)pdata->timrot_base + HW_TIMROT_TIMCTRL_REG(pdata->timrot);
-    regs.uregs[reg_pinctrl] = (long)pdata->pinctrl_base + HW_PINCTRL_DOUT_REG(gpio_bank);
-    regs.uregs[reg_gpiopin] = (long)gpio_pin;
-    regs.uregs[reg_counter] = (long)&hw->counter;
-    set_fiq_regs(&regs);
+	regs.uregs[reg_timctrl] = (long)pdata->timrot_base + HW_TIMROT_TIMCTRL_REG(pdata->timrot);
+	regs.uregs[reg_pinctrl] = (long)pdata->pinctrl_base + HW_PINCTRL_DOUT_REG(gpio_bank);
+	regs.uregs[reg_gpiopin] = (long)gpio_pin;
+	regs.uregs[reg_counter] = (long)&hw->counter;
+	set_fiq_regs(&regs);
 
-    /* change the timer's IRQ to FIR*/
-    irqd = irq_get_irq_data(pdata->timrot_irq);
-    writel(1 << 4,
-       pdata->icoll_base + HW_ICOLL_INTERRUPTn_REG(irqd->hwirq) + BIT_SET);
+	/* change the timer's IRQ to FIR*/
+	irqd = irq_get_irq_data(pdata->timrot_irq);
+	writel(1 << 4,
+	   pdata->icoll_base + HW_ICOLL_INTERRUPTn_REG(irqd->hwirq) + BIT_SET);
 
-    /* enable the FIQ */
-    enable_fiq(pdata->timrot_irq);
+	/* enable the FIQ */
+	enable_fiq(pdata->timrot_irq);
 
-    /* run timer */
-    writel(pdata->timrot_fixed_count,
-        pdata->timrot_base + HW_TIMROT_FIXED_COUNT_REG(pdata->timrot));
+	/* run timer */
+	writel(pdata->timrot_fixed_count,
+		pdata->timrot_base + HW_TIMROT_FIXED_COUNT_REG(pdata->timrot));
 
-    /* setup misc devcie */
-    hw->miscdev.minor  = MISC_DYNAMIC_MINOR;
-    hw->miscdev.name   = dev_name(dev);
-    hw->miscdev.fops   = &imx28_gpio_impulse_gen_fops;
-    hw->miscdev.parent = dev;
+	/* setup misc devcie */
+	hw->miscdev.minor  = MISC_DYNAMIC_MINOR;
+	hw->miscdev.name   = dev_name(dev);
+	hw->miscdev.fops   = &imx28_gpio_impulse_gen_fops;
+	hw->miscdev.parent = dev;
 
-    err = misc_register(&hw->miscdev);
-    if (err) {
-        dev_err(dev, "failed to register misc device\n");
-        goto exit_free_gpio;
-    }
+	err = misc_register(&hw->miscdev);
+	if (err) {
+		dev_err(dev, "failed to register misc device\n");
+		goto exit_free_gpio;
+	}
 
-    dev_info(dev, "registered new misc device %s\n", hw->miscdev.name);
-    return 0;
+	dev_info(dev, "registered new misc device %s\n", hw->miscdev.name);
+	return 0;
 
 exit_free_gpio:
-    gpio_free(pdata->gpio);
+	gpio_free(pdata->gpio);
 exit_free_mem:
-    kfree(hw);
-    if (!dev_get_platdata(&pdev->dev))
-        kfree(pdata);
+	kfree(hw);
+	if (!dev_get_platdata(&pdev->dev))
+		kfree(pdata);
 
-    return err;
+	return err;
 }
 
 static int imx28_gpio_impulse_gen_remove(struct platform_device *pdev)
 {
-    struct imx28_gpio_impulse_gen_hw *hw = platform_get_drvdata(pdev);
-    const struct imx28_gpio_impulse_gen_platform_data *pdata = hw->pdata;
+	struct imx28_gpio_impulse_gen_hw *hw = platform_get_drvdata(pdev);
+	const struct imx28_gpio_impulse_gen_platform_data *pdata = hw->pdata;
 
-    device_init_wakeup(&pdev->dev, false);
+	device_init_wakeup(&pdev->dev, false);
 
-    misc_deregister(&hw->miscdev);
-    gpio_free(pdata->gpio);
-    kfree(hw);
-    if (!dev_get_platdata(&pdev->dev))
-        kfree(pdata);
+	misc_deregister(&hw->miscdev);
+	gpio_free(pdata->gpio);
+	kfree(hw);
+	if (!dev_get_platdata(&pdev->dev))
+		kfree(pdata);
 
-    return 0;
+	return 0;
 }
 
 static struct platform_driver imx28_gpio_impulse_gen_driver = {
-    .probe      = imx28_gpio_impulse_gen_probe,
-    .remove     = imx28_gpio_impulse_gen_remove,
-    .driver     = {
-        .name           = DRV_NAME,
-        .of_match_table = of_match_ptr(imx28_gpio_impulse_gen_of_match),
-    }
+	.probe  = imx28_gpio_impulse_gen_probe,
+	.remove = imx28_gpio_impulse_gen_remove,
+	.driver = {
+		.name           = DRV_NAME,
+		.of_match_table = of_match_ptr(imx28_gpio_impulse_gen_of_match),
+	}
 };
 module_platform_driver(imx28_gpio_impulse_gen_driver);
 
